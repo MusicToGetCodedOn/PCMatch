9d116f37f2b53673724edd7737b214ac01e6ac6d
Loris Pérez <lpe149399@stud.gibb.ch>
2025-04-23 15:36:36 +0200

    Cases und filter geadded

--- PCMatch/src/routes/ProductsRoute.jsx
+++ PCMatch/src/routes/ProductsRoute.jsx
@@ -1,13 +1,42 @@
 import { useState } from "react";
 import GpuCard from "../components/VideoCardCard";
-import gpus from "../data/video-card.json";
+import CpuCard from "../components/CpuCard";
+import CaseCard from "../components/CaseCard";
+
 
 
 export default function ProductsRoute(){
-    const [filter, setFilter] = useState("ALL")
+    const [filter, setFilter] = useState('ALL')
 
-    const filteredProducts = filter === 'ALL'
-    return <main>
-        <GpuCard />
-    </main>
+
+
+    
+
+
+    return (
+        <div>
+      <h2>Produkte</h2>
+      
+        <div style={{ marginBottom: '1rem' }}>
+        <button onClick={() => setFilter('GPU')}>Nur GPUs</button>
+        <button onClick={() => setFilter('CPU')}>Nur CPUs</button>
+        <button onClick={() => setFilter('Case')}>Cases</button>
+
+        </div>
+
+        {filter === 'ALL' && (
+        <>
+          <GpuCard />
+          <CpuCard />
+          <CaseCard />
+        </>
+      )}
+
+      {filter === 'GPU' && <GpuCard />}
+      {filter === 'CPU' && <CpuCard />}
+      {filter === 'Case' && <CaseCard/>}
+        </div>
+    )
+    
+
 }
