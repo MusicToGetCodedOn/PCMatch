64067a6443eb4689091163692e423d063626b9f4
York von Zimmermann <yvo142607@stud.gibb.ch>
2025-04-23 14:47:03 +0200

    HomePage added

--- PCMatch/src/routes/HomeRoute.jsx
+++ PCMatch/src/routes/HomeRoute.jsx
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom';
 import Button from "../components/Button"
 
 export default function HomeRoute(){
-    return <main>
+    return (
+    <main>
+        <div className="info-card">
+            <h1>Welcome to our HomePage</h1>
+            <p>This is just an example Text!</p>
+            <Button><Link to="/builder/">Start your Build</Link> </Button>
+        </div>
+        <div>
+            <h1>Search through Products!</h1>
+            <p>This is not the Final Text!</p>
+            <Button><Link to="/products/">Show Products</Link> </Button>
+        </div>
     </main> 
+    )
 }
