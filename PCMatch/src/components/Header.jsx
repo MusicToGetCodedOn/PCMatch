c7fd7e88634c6d9bdce03cdccd0a73889c0af630
York von Zimmermann <yvo142607@stud.gibb.ch>
2025-04-24 10:51:39 +0200

    Home Button To Logo

--- PCMatch/src/components/Header.jsx
+++ PCMatch/src/components/Header.jsx
@@ -4,14 +4,13 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-       <h1 className={styles.headerTitle}>PCMatch</h1>
+       <Link to="/" className={styles.headerTitle}>PCMatch</Link>
 
       <br />
       <nav className={styles.nav}>
       <Link to="/builder/" className={styles.homeButton}>Builder</Link>
       <Link to="/completedbuilds/" className={styles.homeButton}>Completed Builds</Link>
       <Link to="/products/" className={styles.homeButton}>Products</Link>
-      <Link to="/" className={styles.homeButton}>Home</Link>
       
     
       </nav>
