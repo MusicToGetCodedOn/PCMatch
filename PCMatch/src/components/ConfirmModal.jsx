273ef86d9715cdc6ec9f0ff7b27ae6e5795167e0
Loris Pérez <lpe149399@stud.gibb.ch>
2025-04-29 18:43:40 +0200

    Delete function fixed

--- PCMatch/src/components/ConfirmModal.jsx
+++ PCMatch/src/components/ConfirmModal.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import styles from "./ConfirmModal.css";
+import styles from './ConfirmModal.module.css';
 
-const ConfirmModal = ({ message, onConfirm, onCancel }) => {
+export default function ConfirmModal({ message, onConfirm, onCancel }) {
   return (
-    <div className={styles.modal-overlay}>
-      <div className={styles.modal-box}>
+    <div className={styles["modal-overlay"]}>
+      <div className={styles["modal-box"]}>
         <p>{message}</p>
-        <div className={styles.modal-buttons}>
-          <button className={styles.cancel-button} onClick={onCancel}>Abbrechen</button>
-          <button className={styles.confirm-button} onClick={onConfirm}>Löschen</button>
+        <div className={styles["modal-buttons"]}>
+          <button className={styles["cancel-button"]} onClick={onCancel}>Abbrechen</button>
+          <button className={styles["confirm-button"]} onClick={onConfirm}>Löschen</button>
         </div>
       </div>
     </div>
   );
-};
-
-export default ConfirmModal;
+}
